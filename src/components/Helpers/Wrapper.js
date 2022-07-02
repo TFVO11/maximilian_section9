0a87b89bff460cd611c124910e4668813819c734
TFVO11 <cosmoyj@naver.com>
2022-07-02 12:17:02 +0900

    sction9_2

---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,7 @@
+const Wrapper = props => {
+    return props.children;
+};
+
+export default Wrapper;
+
+//props.children is hold all the contents that parent components's jsx inside of this Wrapper
